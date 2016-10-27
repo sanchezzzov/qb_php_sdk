e1048cbee0248f7c4861ae55ad2fd54955cbe6d5
Alex <kirilyuk_aa@cvek.ru>
2016-10-27 10:43:58 +0300

    Disconnect Action. Create common class QuickBooksService.

--- actions/DisconnectAction.php
+++ actions/DisconnectAction.php
@@ -2,40 +2,31 @@
 /**
  * Created by PhpStorm.
  * User: alex1
- * Date: 26.10.16
- * Time: 16:21
+ * Date: 27.10.16
+ * Time: 10:36
  */
 
-namespace qb_php_sdk;
+namespace qb_php_sdk\actions;
 
 
 use qb_php_sdk\Core\IntuitServicesType;
 use qb_php_sdk\Core\ServiceContext;
-use qb_php_sdk\DataService\DataService;
+use qb_php_sdk\PlatformService\PlatformService;
 use qb_php_sdk\Security\OAuthRequestValidator;
+use yii\base\Action;
 use yii\helpers\ArrayHelper;
 
-class QuickBooksService
+class DisconnectAction extends Action
 {
-    /**
-     * @var \qb_php_sdk\DataService\DataService
-     */
-    protected $dataService;
+    public $redirectUrl = ['index'];
 
-    public function __construct()
-    {
-        $this->dataService = $this->resolveDataService();
-    }
-
-    private function resolveDataService()
+    public function run()
     {
         $serviceType = IntuitServicesType::QBO;
-        $realmId     = \Yii::$app->session->get('realmId');
-
+        $realmId = \Yii::$app->session->get('realmId');
         if (!$realmId) {
             throw new \Exception("RealmID is not set");
         }
-
         $token            = unserialize(\Yii::$app->session->get('token'));
         $requestValidator = new OAuthRequestValidator(ArrayHelper::getValue($token, 'oauth_token'),
             ArrayHelper::getValue($token, 'oauth_token_secret'), OAUTH_CONSUMER_KEY, OAUTH_CONSUMER_SECRET);
@@ -43,13 +34,12 @@ class QuickBooksService
         if (!$serviceContext) {
             throw new \Exception("Problem while initializing ServiceContext.\n");
         }
-        $dataService = new DataService($serviceContext);
-        if (!$dataService) {
-            throw new \Exception("Problem while initializing DataService.\n");
+        $platformService = new PlatformService($serviceContext);
+        $platformService->Disconnect();
+        \Yii::$app->session->remove('token');
+        \Yii::$app->session->remove('realmId');
+        \Yii::$app->session->remove('secret');
 
-        }
-
-        return $dataService;
+        return $this->controller->redirect($this->redirectUrl);
     }
-
 }
